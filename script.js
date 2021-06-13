a57256839b0946f4bd1a898249f325c3f1b9644c
wangsh <wugcelama19@gmail.com>
2021-06-13 14:36:32 +0545

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+//function create.
+
+function myNumber(){
+
+    let myBike = {
+
+        name : 'Kawasaki',
+
+        model : 'A124',
+
+        release : 2015,
+
+        design : 'patter'
+    }
+
+    for(let i in myBike){
+
+        console.log(myBike[i]);
+    }
+
+    
+}
+
+myNumber();
